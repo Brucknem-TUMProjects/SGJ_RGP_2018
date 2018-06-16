33533f2daff14c849966c62caddc6ca1e7833028
Akbar Suriaganda <akbar.suriaganda@gmail.com>
2018-06-16 22:25:14 +0200

    Mesh added to player

--- NightmaZe/Assets/Scripts/Player.cs
+++ NightmaZe/Assets/Scripts/Player.cs
@@ -5,306 +5,311 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
 
-	public enum Posture
-	{
-		standing = 0,
-		crouching = 1,
-		proning = 2,
-	};
+    public enum Posture
+    {
+        standing = 0,
+        crouching = 1,
+        proning = 2,
+    };
 
-	[System.Serializable]
-	public class MoveSettings
-	{
-		public float runMultiplier = 1.3f;
-		public float walkVelocity = 20;
-		public float crouchVelocity = 15;
-		public float proneVelocity = 5;
-		public float jumpVelocity = 5f;
-		public float rotationSpeed = 7;
-		public float distanceToGround = 1.3f;
-		public LayerMask ground;
-	}
+    [System.Serializable]
+    public class MoveSettings
+    {
+        public float runMultiplier = 1.3f;
+        public float walkVelocity = 20;
+        public float crouchVelocity = 15;
+        public float proneVelocity = 5;
+        public float jumpVelocity = 5f;
+        public float rotationSpeed = 7;
+        public float distanceToGround = 1.3f;
+        public LayerMask ground;
+    }
 
-	#region parameters
-	public MoveSettings moveSettings;
-	public Transform spawnpoint;
-	public float holdTimeForProne = 1.0f;
-	public Collider[] postureCollider;          //0: Standing, 1:Crouching, 2:Proning
-	#endregion
+    #region parameters
+    public MoveSettings moveSettings;
+    public Transform spawnpoint;
+    public float holdTimeForProne = 1.0f;
+    public Collider[] postureCollider;          //0: Standing, 1:Crouching, 2:Proning
+    public GameObject camera;
+    #endregion
 
-	#region private attributes
-	private Rigidbody playerRigid;
-	private Vector3 velocity;
-	private GameObject camera;
+    #region private attributes
+    private Rigidbody playerRigid;
+    private Vector3 velocity;
 
-	private float downTime;
-	private float speedValue;
-	private Posture currentPosture;
-	private bool skipButtonUpEvent, stopReadingInput;
-	private Vector3 postureHeights;
+    private float downTime;
+    private float speedValue;
+    private Posture currentPosture;
+    private bool skipButtonUpEvent, stopReadingInput;
+    private Vector3 postureHeights;
 
-	// Inputs
-	bool jumpInput;
+    // Inputs
+    bool jumpInput;
 
-	#endregion
+    #endregion
 
-	// Reset attributes to default values
-	public void Reset()
-	{
-		velocity = Vector3.zero;
-		downTime = 0;
-		speedValue = moveSettings.walkVelocity;
-		jumpInput = skipButtonUpEvent = false;
-		currentPosture = Posture.standing;
-		postureCollider[0].enabled = true;
-		postureCollider[1].enabled = false;
-		postureCollider[2].enabled = false;
-	}
+    // Reset attributes to default values
+    public void Reset()
+    {
+        velocity = Vector3.zero;
+        downTime = 0;
+        speedValue = moveSettings.walkVelocity;
+        jumpInput = skipButtonUpEvent = false;
+        currentPosture = Posture.standing;
+        postureCollider[0].enabled = true;
+        postureCollider[1].enabled = false;
+        postureCollider[2].enabled = false;
+    }
 
-	// Check if player is on ground
-	bool OnGround()
-	{
-		return Physics.Raycast(transform.position, Vector3.down, moveSettings.distanceToGround, moveSettings.ground);
-	}
+    // Check if player is on ground
+    bool OnGround()
+    {
+        return Physics.Raycast(transform.position + Vector3.up * .1f, Vector3.down, moveSettings.distanceToGround, moveSettings.ground);
+    }
 
-	bool CheckForCeiling(float height)
-	{
-		return Physics.Raycast(transform.position, Vector3.up, height, 10);
-	}
+    bool CheckForCeiling(float height)
+    {
+        return Physics.Raycast(transform.position, Vector3.up, height, 10);
+    }
 
-	// Use this for initialization
-	void Start()
-	{
-		// ---TODO: Spawn, Reset
-		playerRigid = GetComponent<Rigidbody>();
-		camera = transform.GetChild(0).gameObject;
-		camera.GetComponent<MouseLook>().Init(this.transform, camera.transform);
-		Reset();
-		Cursor.visible = false;
-		Cursor.lockState = CursorLockMode.Locked;
-		//Physics.gravity = Vector3.down * 20;
-	}
+    // Use this for initialization
+    void Start()
+    {
+        // ---TODO: Spawn, Reset
+        playerRigid = GetComponent<Rigidbody>();
+        //camera = transform.GetChild(0).gameObject;
+        camera.GetComponent<MouseLook>().Init(this.transform, camera.transform);
+        Reset();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        //Physics.gravity = Vector3.down * 20;
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-		camera.GetComponent<MouseLook>().LookRotation(this.transform, camera.transform);
-		//CheckLife();
-		//GetTimer();
-		//if (!ableToMove)
-		//    return;
-		GetJumpInput();
-		GetButtonInput();
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        camera.GetComponent<MouseLook>().LookRotation(this.transform, camera.transform);
+        //CheckLife();
+        //GetTimer();
+        //if (!ableToMove)
+        //    return;
+        GetJumpInput();
+        GetButtonInput();
+    }
 
-	void FixedUpdate()
-	{
-		Move();
-		if (jumpInput)
-			Jump();
-	}
+    void FixedUpdate()
+    {
+        Move();
+        if (jumpInput)
+            Jump();
+    }
 
-	void GetJumpInput()
-	{
-		if (Input.GetButtonDown("Jump"))
-			jumpInput = true;
-	}
+    void GetJumpInput()
+    {
+        if (Input.GetButtonDown("Jump"))
+            jumpInput = true;
+    }
 
-	void Move()
-	{
-		velocity = new Vector3(Input.GetAxis("Horizontal"), playerRigid.velocity.y, Input.GetAxis("Vertical"));
-		velocity *= speedValue;
-		if (Input.GetAxis("RT") > 0)
-		{
-			if(velocity.z > 0)
-				velocity.z *= moveSettings.runMultiplier;
-		}
-		// Default
-		velocity.y = playerRigid.velocity.y;
-		playerRigid.velocity = Vector3.Lerp(playerRigid.velocity, transform.TransformDirection(velocity), Time.fixedDeltaTime * 7f);
-	}
-
-
-	void GetButtonInput()
-	{
-		// Crouching, Proning = Hold Crouch Button	
-		if (Input.GetButton("ControllerXButton"))
-		{
-			if (!stopReadingInput)
-			{
-				downTime += Time.deltaTime;
-				skipButtonUpEvent = false;
-			}
-			else
-				skipButtonUpEvent = true;
-		}
-		if (Input.GetButtonUp("ControllerXButton") || downTime >= holdTimeForProne)
-		{
-			if (downTime < holdTimeForProne && !skipButtonUpEvent)
-			{
-				// User pressing Crouch Button for a shorter time then needed for prone
-				// Possible: Player is either standing (-> crouch), crouching (-> stand up), proning(-> crouching)
-				if (currentPosture == Posture.standing)
-				{
-					Crouch();
-				}
-				else if (currentPosture == Posture.crouching)
-				{
-					StandUp();
-				}
-				else if (currentPosture == Posture.proning)
-				{
-					Crouch();
-				}
-			}
-			else if (!stopReadingInput)
-			{
-				// User holds Crouch Button long enough for prone
-				//Possible: Player either standing(->prone), crouching(->prone), proning(->standup)
-				downTime = 0;
-				skipButtonUpEvent = true;
-				stopReadingInput = true;
-				if (currentPosture == Posture.standing)
-				{
-					Prone();
-				}
-				else if (currentPosture == Posture.crouching)
-				{
-					Prone();
-				}
-				else if (currentPosture == Posture.proning)
-				{
-					StandUp();
-				}
-			}
-
-			if (Input.GetButtonUp("ControllerXButton"))
-			{
-				stopReadingInput = false;
-				skipButtonUpEvent = false;
-			}
-			downTime = 0;
-		}
-	}
+    void Move()
+    {
+        velocity = new Vector3(Input.GetAxis("Horizontal"), playerRigid.velocity.y, Input.GetAxis("Vertical"));
+        velocity *= speedValue;
+        if (Input.GetAxis("RT") > 0)
+        {
+            if (velocity.z > 0)
+                velocity.z *= moveSettings.runMultiplier;
+        }
+        // Default
+        velocity.y = playerRigid.velocity.y;
+        playerRigid.velocity = Vector3.Lerp(playerRigid.velocity, transform.TransformDirection(velocity), Time.fixedDeltaTime * 7f);
+    }
 
 
+    void GetButtonInput()
+    {
+        // Crouching, Proning = Hold Crouch Button	
+        if (Input.GetButton("ControllerXButton"))
+        {
+            if (!stopReadingInput)
+            {
+                downTime += Time.deltaTime;
+                skipButtonUpEvent = false;
+            }
+            else
+                skipButtonUpEvent = true;
+        }
+        if (Input.GetButtonUp("ControllerXButton") || downTime >= holdTimeForProne)
+        {
+            if (downTime < holdTimeForProne && !skipButtonUpEvent)
+            {
+                // User pressing Crouch Button for a shorter time then needed for prone
+                // Possible: Player is either standing (-> crouch), crouching (-> stand up), proning(-> crouching)
+                if (currentPosture == Posture.standing)
+                {
+                    Crouch();
+                }
+                else if (currentPosture == Posture.crouching)
+                {
+                    StandUp();
+                }
+                else if (currentPosture == Posture.proning)
+                {
+                    Crouch();
+                }
+            }
+            else if (!stopReadingInput)
+            {
+                // User holds Crouch Button long enough for prone
+                //Possible: Player either standing(->prone), crouching(->prone), proning(->standup)
+                downTime = 0;
+                skipButtonUpEvent = true;
+                stopReadingInput = true;
+                if (currentPosture == Posture.standing)
+                {
+                    Prone();
+                }
+                else if (currentPosture == Posture.crouching)
+                {
+                    Prone();
+                }
+                else if (currentPosture == Posture.proning)
+                {
+                    StandUp();
+                }
+            }
 
-	bool StandUp()
-	{
-		// Check if standing up is possible
-		bool canStandUp = true;
-		if (currentPosture == Posture.crouching)
-		{
-			canStandUp = !CheckForCeiling(0.9f);
-		}
-		else if (currentPosture == Posture.proning)
-		{
-			canStandUp = !CheckForCeiling(1.3f);
-		}
+            if (Input.GetButtonUp("ControllerXButton"))
+            {
+                stopReadingInput = false;
+                skipButtonUpEvent = false;
+            }
+            downTime = 0;
+        }
+    }
 
-		// Perform standing up
-		if (!canStandUp)
-		{
-			Debug.Log("Cannot stand up, ceiling too low! Du hasch koin blatz nedda, bisch zu fett");
-			return false;
-		}
 
-		// TODO: Switch Colliders, animate Character 
-		postureCollider[0].enabled = true;
-		postureCollider[1].enabled = false;
-		postureCollider[2].enabled = false;
-		Debug.Log("Stood up");
-		currentPosture = Posture.standing;
-		speedValue = moveSettings.walkVelocity;
-		return true;
-	}
 
-	bool Crouch()
-	{
-		if (currentPosture == Posture.proning)
-		{
-			if (CheckForCeiling(0.1f))
-			{
-				// TODO: Switch Colliders, animate Character
-				Debug.Log("Cannot go to crouch from prone!");
-				return false;
-			}
-		}
+    bool StandUp()
+    {
+        // Check if standing up is possible
+        bool canStandUp = true;
+        if (currentPosture == Posture.crouching)
+        {
+            canStandUp = !CheckForCeiling(0.9f);
+        }
+        else if (currentPosture == Posture.proning)
+        {
+            canStandUp = !CheckForCeiling(1.3f);
+        }
 
-		postureCollider[0].enabled = false;
-		postureCollider[1].enabled = true;
-		postureCollider[2].enabled = false;
-		currentPosture = Posture.crouching;
-		speedValue = moveSettings.crouchVelocity;
-		Debug.Log("Going into Crouch");
-		return true;
-	}
+        // Perform standing up
+        if (!canStandUp)
+        {
+            Debug.Log("Cannot stand up, ceiling too low! Du hasch koin blatz nedda, bisch zu fett");
+            return false;
+        }
 
-	bool Prone()
-	{
-		// TODO: Switch Collider, animate Character to prone
-		postureCollider[0].enabled = false;
-		postureCollider[1].enabled = false;
-		postureCollider[2].enabled = true;
-		currentPosture = Posture.proning;
-		speedValue = moveSettings.proneVelocity;
-		// Nice to have: Check if proning is possible! (?)
-		return true;
-	}
-    
-	// FixedUpdate/Physics methods
-	void Jump()
-	{
-		if (OnGround() && currentPosture == Posture.standing)
-		{
-			playerRigid.velocity = new Vector3(playerRigid.velocity.x, moveSettings.jumpVelocity, playerRigid.velocity.z);
-		}
-		jumpInput = false;
-	}
+        // TODO: Switch Colliders, animate Character 
+        postureCollider[0].enabled = true;
+        postureCollider[1].enabled = false;
+        postureCollider[2].enabled = false;
+        Debug.Log("Stood up");
+        currentPosture = Posture.standing;
+        speedValue = moveSettings.walkVelocity;
+        return true;
+    }
 
-	// Collision/Trigger methods
-	void OnTriggerEnter(Collider col)
-	{
-		switch (col.tag)
-		{
-			#region trigger
-			case "DeathZone":
-				Debug.Log("Player Dieded");
-				break;
-			case "Respawn":
-				spawnpoint = col.transform;
-				break;
-				#endregion
-		}
-	}
-	void OnTriggerStay(Collider col)
-	{
+    bool Crouch()
+    {
+        if (currentPosture == Posture.proning)
+        {
+            if (CheckForCeiling(0.1f))
+            {
+                // TODO: Switch Colliders, animate Character
+                Debug.Log("Cannot go to crouch from prone!");
+                return false;
+            }
+        }
 
-	}
+        postureCollider[0].enabled = false;
+        postureCollider[1].enabled = true;
+        postureCollider[2].enabled = false;
+        currentPosture = Posture.crouching;
+        speedValue = moveSettings.crouchVelocity;
+        Debug.Log("Going into Crouch");
+        return true;
+    }
 
-	void OnTriggerExit(Collider col)
-	{
+    bool Prone()
+    {
+        // TODO: Switch Collider, animate Character to prone
+        postureCollider[0].enabled = false;
+        postureCollider[1].enabled = false;
+        postureCollider[2].enabled = true;
+        currentPosture = Posture.proning;
+        speedValue = moveSettings.proneVelocity;
+        // Nice to have: Check if proning is possible! (?)
+        return true;
+    }
 
-	}
+    // FixedUpdate/Physics methods
+    void Jump()
+    {
+        if (OnGround() && currentPosture == Posture.standing)
+        {
+            playerRigid.velocity = new Vector3(playerRigid.velocity.x, moveSettings.jumpVelocity, playerRigid.velocity.z);
+        }
+        jumpInput = false;
+    }
 
-	// public methods
-	public void Spawn()
-	{
-		transform.position = spawnpoint.position;
-	}
+    // Collision/Trigger methods
+    void OnTriggerEnter(Collider col)
+    {
+        switch (col.tag)
+        {
+            #region trigger
+            case "DeathZone":
+                Debug.Log("Player Dieded");
+                break;
+            case "Respawn":
+                spawnpoint = col.transform;
+                break;
+                #endregion
+        }
+    }
+    void OnTriggerStay(Collider col)
+    {
 
-	public void Death()
-	{
-		// ---TODO: Action on death
-		StartCoroutine(OnDeath());
-	}
+    }
 
-	IEnumerator OnDeath()
-	{
-		//ableToMove = false;
-		//yield return new WaitForSeconds(3f);
-		//ui.SetAlpha(1);
-		//Travel.SetDestination("GameOver");
-		yield return null;
-	}
+    void OnTriggerExit(Collider col)
+    {
+
+    }
+
+    // public methods
+    public void Spawn()
+    {
+        transform.position = spawnpoint.position;
+    }
+
+    public void Death()
+    {
+        // ---TODO: Action on death
+        StartCoroutine(OnDeath());
+    }
+
+    IEnumerator OnDeath()
+    {
+        //ableToMove = false;
+        //yield return new WaitForSeconds(3f);
+        //ui.SetAlpha(1);
+        //Travel.SetDestination("GameOver");
+        yield return null;
+    }
+
+    public Posture GetCurrentPosture()
+    {
+        return currentPosture;
+    }
 }
---
+++ NightmaZe/Assets/_Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    [SerializeField]
+    Player player;
+    [SerializeField]
+    Rigidbody rigid;
+    Animator anim;
+
+    [Space]
+    [SerializeField]
+    float transitionSpeed;
+
+    // Use this for initialization
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float speed = rigid.velocity.magnitude;
+        float toPose;
+        Player.Posture posture = player.GetCurrentPosture();
+        switch (posture)
+        {
+            case Player.Posture.standing:
+                speed /= player.moveSettings.walkVelocity * player.moveSettings.runMultiplier;
+                toPose = 1;
+                break;
+            case Player.Posture.crouching:
+                speed /= player.moveSettings.crouchVelocity * player.moveSettings.runMultiplier;
+                toPose = .5f;
+                break;
+            default:
+                speed /= player.moveSettings.proneVelocity * player.moveSettings.runMultiplier;
+                toPose = 0;
+                break;
+        }
+        anim.SetFloat("Speed", speed);
+        anim.SetFloat("Pose", Mathf.Lerp(anim.GetFloat("Pose"), toPose, Time.fixedDeltaTime * transitionSpeed));
+    }
+
+    public void Jump()
+    {
+        anim.SetTrigger("Jump");
+    }
+}
