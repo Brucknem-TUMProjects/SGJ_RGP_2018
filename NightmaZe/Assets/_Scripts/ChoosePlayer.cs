c5103d87f290e05e493f40a46448c34edc1f6a55
Brucknem <mbruckner94@gmail.com>
2018-06-17 08:10:27 +0200

    Dropdown

---
+++ NightmaZe/Assets/_Scripts/ChoosePlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ChoosePlayer : MonoBehaviour {
+
+    public Dropdown selector;
+
+    public GameObject[] prefabs;
+
+	// Use this for initialization
+	void Start () {
+        selector.onValueChanged.AddListener(delegate { Listener(selector); });
+	}
+
+    void Listener(Dropdown dropdown)
+    {
+        Debug.Log("listener");
+        GetComponent<NetworkLobbyManager>().gamePlayerPrefab = prefabs[dropdown.value];
+    }
+}
