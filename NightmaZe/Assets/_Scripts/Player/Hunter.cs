c4afb8bd4253042e5141177064a3982d82b95509
Marian Ludwig <ga86lop@mytum.de>
2018-06-17 06:28:02 +0200

    Hunter fix

--- NightmaZe/Assets/_Scripts/Player/Hunter.cs
+++ NightmaZe/Assets/_Scripts/Player/Hunter.cs
@@ -5,45 +5,31 @@ using UnityEngine.Networking;
 
 public class Hunter : NetworkBehaviour
 {
-<<<<<<< HEAD
-    [SerializeField]
-    PlayerAnimator anim;
-=======
-    [SyncVar]
+	[SerializeField]
+	PlayerAnimator anim;
+	[SyncVar]
 	float cooldown = 10.0f;
-    [SyncVar]
+	[SyncVar]
 	float timer;
->>>>>>> MergePlayers
 
-    float cooldown = 10.0f;
-    float timer;
-
-<<<<<<< HEAD
-    Player.MoveSettings moveSettingsBoosted = new Player.MoveSettings();
-
-    void Start()
-    {
-        timer = Time.time - cooldown;
-    }
-=======
 	void Start()
 	{
-        if (!isLocalPlayer)
-        {
-            return;
-        }
+		if (!isLocalPlayer)
+		{
+			return;
+		}
 
-        timer = Time.time - cooldown;
+		timer = Time.time - cooldown;
 	}
 
 	void Update()
 	{
-        if (!isLocalPlayer)
-        {
-            return;
-        }
+		if (!isLocalPlayer)
+		{
+			return;
+		}
 
-        if (timer + cooldown < Time.time)
+		if (timer + cooldown < Time.time)
 		{
 			Debug.Log("Timer ready");
 			if (Input.GetAxisRaw("VerticalSteuerkreuz") < 0)
@@ -55,54 +41,37 @@ public class Hunter : NetworkBehaviour
 				// Steuerkreuz nach oben --> Boost speed for 5 secs
 				Debug.Log("Move BoosT");
 				StartCoroutine(BoostSpeed());
->>>>>>> MergePlayers
+			}
+			else if (Input.GetAxisRaw("HorizontalSteuerkreuz") > 0)
+			{
+				// Steuerkreuz nach links
+			}
+			else if (Input.GetAxisRaw("HorizontalSteuerkreuz") < 0)
+			{
+				// Steuerkreuz nach rechts
+			}
+			else
+			{
+				return;
+			}
+			timer = Time.time;
+		}
+		if (Input.GetButtonDown("InteractRight"))
+		{
+			Attack();
+		}
+	}
 
-    void Update()
-    {
-        if (timer + cooldown < Time.time)
-        {
-            Debug.Log("Timer ready");
-            if (Input.GetAxisRaw("VerticalSteuerkreuz") < 0)
-            {
-                // Steuerkreuz nach unten
-            }
-            else if (Input.GetAxisRaw("VerticalSteuerkreuz") > 0)
-            {
-                // Steuerkreuz nach oben --> Boost speed for 5 secs
-                Debug.Log("Move BoosT");
-                StartCoroutine(BoostSpeed());
+	void Attack()
+	{
+		if (!anim.attacking)
+			anim.Punch();
+	}
 
-            }
-            else if (Input.GetAxisRaw("HorizontalSteuerkreuz") > 0)
-            {
-                // Steuerkreuz nach links
-            }
-            else if (Input.GetAxisRaw("HorizontalSteuerkreuz") < 0)
-            {
-                // Steuerkreuz nach rechts
-            }
-            else
-            {
-                return;
-            }
-            timer = Time.time;
-        }
-        if (Input.GetButtonDown("InteractRight"))
-        {
-            Attack();
-        }
-    }
-
-    void Attack()
-    {
-        if (!anim.attacking)
-            anim.Punch();
-    }
-
-    IEnumerator BoostSpeed()
-    {
-        GetComponent<Player>().speedBoost = 2;
-        yield return new WaitForSeconds(5);
-        GetComponent<Player>().speedBoost = 1;
-    }
+	IEnumerator BoostSpeed()
+	{
+		GetComponent<Player>().speedBoost = 2;
+		yield return new WaitForSeconds(5);
+		GetComponent<Player>().speedBoost = 1;
+	}
 }
